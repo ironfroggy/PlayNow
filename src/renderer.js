8d6da13fc019acb5779c39d7358c3b72467c02a0
Calvin Spealman <calvin@pantechnoco.com>
2011-09-02 07:23:35 -0400

    Wrap rendering code into a closure and add a utility colorStyle function only used in rendering

--- src/renderer.js
+++ src/renderer.js
@@ -1,31 +1,45 @@
-var renderer = new Behavior('position');
-renderer.x = 0;
-renderer.y = 0;
-renderer.zoom = 1;
-renderer.bind('entitytick', function(e, t, entity) {
-    var position = entity.get('position')
-    ,   color = entity.get('color')
-    ,   size = 10
-    ;
+var renderer;
 
-    ctx.save();
+(function(){
+    renderer = new Behavior('position');
+    
+    renderer.x = 0;
+    renderer.y = 0;
+    renderer.zoom = 1;
 
-    ctx.translate(position[0], position[1]);
+    renderer.bind('entitytick', function(e, t, entity) {
+        var position = entity.get('position')
+        ,   color = entity.get('color')
+        ,   size = 10
+        ;
 
-    ctx.fillStyle = ['rgb(', parseInt(color[0]*255), ', ', parseInt(color[1]*255), ', ', parseInt(color[2]*255), ')'].join('');
-    ctx.fillRect(-size/2, -size/2, size, size);
+        ctx.save();
 
-    ctx.restore();
-});
-renderer.bind('beforetick', function() {
-    ctx.save();
+        ctx.translate(position[0], position[1]);
 
-    ctx.scale(this.zoom, this.zoom);
-    ctx.translate(this.x, this.y);
+        ctx.fillStyle = colorStyle(color);
+        ctx.fillRect(-size/2, -size/2, size, size);
 
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-    ctx.fillRect(0, 0, 640, 480);
-});
-renderer.bind('aftertick', function() {
-    ctx.restore();
-});
+        ctx.restore();
+    });
+    renderer.bind('beforetick', function() {
+        ctx.save();
+
+        ctx.scale(this.zoom, this.zoom);
+        ctx.translate(this.x, this.y);
+
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+        ctx.fillRect(0, 0, 640, 480);
+    });
+    renderer.bind('aftertick', function() {
+        ctx.restore();
+    });
+
+    // render utilities
+    function colorStyle(color) {
+        if (color.length === 3)
+            return ['rgb(', parseInt(color[0]*255), ', ', parseInt(color[1]*255), ', ', parseInt(color[2]*255), ')'].join('');
+        else
+            return ['rgba(', parseInt(color[0]*255), ', ', parseInt(color[1]*255), ', ', parseInt(color[2]*255), ', ', parseInt(color[3]*255), ')'].join('');
+    }
+})();
