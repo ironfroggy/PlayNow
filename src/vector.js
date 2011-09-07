08e0d995e719222e3040d05d613dfbdb6860a31d
Calvin Spealman <calvin@pantechnoco.com>
2011-09-06 23:38:08 -0400

    Add an R type for regions with an intersect method

--- src/vector.js
+++ src/vector.js
@@ -1,6 +1,8 @@
 function V(x, y) {
-    this.push(x);
-    this.push(y);
+    this[0] = x;
+    this[1] = y;
+    this.x = x;
+    this.y = y;
 }
 V.prototype = new Array();
 V.prototype.add = function(b) {
@@ -20,4 +22,32 @@ V.prototype.normal = function() {
 };
 V.prototype.multiply = function(n) {
     return new V(this[0] * n, this[1] * n, this[2] * n);
+};
+
+
+function R(x, y, w, h) {
+    V.call(this, x, y);
+    this[2] = w;
+    this[3] = h;
+    this.w = w;
+    this.h = h;
+};
+R.prototype = new V(0, 0);
+R.prototype.intersects = function(b) {
+    if (b instanceof R) {
+        return (!(
+            this[0] > (b[0]+b[2]) ||
+            this[0]+this[2] < b[0] ||
+            this[1] > (b[1]+b[3]) ||
+            this[1]+this[3] < b[1]
+        ));
+    } else {
+        var i=0, l=b.length, c=[];
+        for (; i<l; i++) {
+            if (this.is_in(b[i])) {
+                c.push(b[i]);
+            }
+        }
+        return c;
+    }
 };
