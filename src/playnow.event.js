cf721ac35575a287680b4aec2a4005779b669158
Calvin Spealman <calvin@ironfroggy.com>
2011-09-09 21:59:13 -0400

    Add a bindonce() to set an event that can only be triggered a single time

--- src/playnow.event.js
+++ src/playnow.event.js
@@ -1,5 +1,5 @@
-function EventHandling() {
-}
+function EventHandling() {}
+
 EventHandling.prototype.bind = function(eventname, callback) {
     if (typeof this.__event_handlers === 'undefined') {
         this.__event_handlers = {};
@@ -7,8 +7,19 @@ EventHandling.prototype.bind = function(eventname, callback) {
     if (typeof this.__event_handlers[eventname] === 'undefined') {
         this.__event_handlers[eventname] = [];
     }
+
+
+    if (typeof this.__event_flags !== 'undefined') {
+        if (this.__event_flags[eventname] instanceof Array) {
+            var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+            args.unshift(eventname);
+            callback.apply(this, args);
+            return
+        }
+    }
     this.__event_handlers[eventname].push(callback);
 }
+
 EventHandling.prototype.unbind = function(eventname, callback) {
     if (typeof this.__event_handlers === 'undefined') {
         return 0;
@@ -27,11 +38,28 @@ EventHandling.prototype.unbind = function(eventname, callback) {
     return r;
 }
 
+EventHandling.prototype.bindonce = function (eventname, callback) {
+    if (typeof this.__event_flags === 'undefined') {
+        this.__event_flags = {};
+    }
+
+    this.__event_flags[eventname] = false;
+    this.bind(eventname, callback);
+};
+
 EventHandling.prototype.trigger = function (eventname) {
     var args = Array.prototype.slice.call(arguments, 1, arguments.length)
     ,   event_name_parts = eventname.split(".")
     ;
     args.unshift(eventname);
+
+    if (typeof this.__event_flags !== 'undefined') {
+        if (this.__event_flags[eventname] instanceof Array) {
+            return;
+        } else if (this.__event_flags[eventname] === false) {
+            this.__event_flags[eventname] = args;
+        }
+    }
 
     for (var i=1, l=event_name_parts.length; i<=l; i++) {
         trigger_specific.call(this, event_name_parts.slice(0, i).join('.'));
