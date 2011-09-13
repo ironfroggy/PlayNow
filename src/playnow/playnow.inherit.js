37aa35a7613e8e4711e996cfa654cade5af01d39
Calvin Spealman <calvin@pantechnoco.com>
2011-09-13 00:00:14 -0400

    Defining a better type helper.

--- src/playnow/playnow.inherit.js
+++ src/playnow/playnow.inherit.js
@@ -1,11 +1,27 @@
-now.type = function(params) {
-    constructor = function() {
-        params.init.apply(this, arguments);
+(function(now){
+    var default_type_params = {
+        toString: function() {
+            return "<"+this.constructor.__name__ +" instance>";
+        }
     };
-    constructor.prototype = new params.inherit();
-    constructor.prototype.constructor = constructor
-    for (param in params) {
-        constructor.prototype[param] = params[param];
-    }
-    return constructor;
-};
+
+    now.type = function(name, params) {
+        constructor = function() {
+            params.init.apply(this, arguments);
+        };
+        constructor.prototype = new params.inherit();
+        constructor.prototype.constructor = constructor
+        constructor.__name__ = name;
+        constructor.toString = function() {
+            return "<"+ name +" constructor>";
+        }
+
+        for (param in default_type_params) {
+            constructor.prototype[param] = default_type_params[param];
+        }
+        for (param in params) {
+            constructor.prototype[param] = params[param];
+        }
+        return constructor;
+    };
+})(now);
